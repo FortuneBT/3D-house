541ea47361c185197b173f2e7a26613a8c090fda
FortuneBT <fortune.tshali@gmail.com>
2022-02-28 10:47:55 +0100

    adding myJson class in Myjson.py

---
+++ Utils/myJson.py
@@ -0,0 +1,67 @@
+import rasterio as rio
+import json
+
+class myJson():
+
+    def create_coord_json(self):
+
+        mylist = self.create_list_files()
+
+        data = dict()
+        data["parameters"] = []
+
+        for elem in mylist:
+
+            myfile = rio.open(elem)
+
+            path = myfile.name
+
+            file = path.split("/")[8]
+            file = file.split(".")[0]
+
+            big_left:float = myfile.bounds.left
+            big_top:float = myfile.bounds.top
+            big_bottom:float= myfile.bounds.bottom
+            big_right:float = myfile.bounds.right
+
+            data["parameters"].append({
+                "Left" : big_left,
+                "Bottom" : big_bottom,
+                "Right" : big_right,
+                "Top" : big_top,
+                "Name" : file,
+                "Path" : path
+            })
+
+        with open("mycoord.json", "w") as mydata:
+            json.dump(data,mydata)
+    
+
+def read_json():
+    with open("mycoord.json") as file:
+        data = json.load(file)
+    
+    return data["parameters"]
+
+
+
+def create_list_files():
+    """
+    Create a variable that contains the liste of files that i have to read
+    """
+
+    my_files:List= []
+    mynumber:str = ""
+    extension = ".tif"
+
+    for x in range(1,44):
+        if x < 10:
+            mynumber = "0"+str(x)
+            mypath = f"zip+file:.///geo/DHMVIIDSMRAS1m_k{mynumber}.zip!/GeoTIFF/"
+        else:
+            mynumber = str(x)
+            mypath = f"zip+file:.///geo/DHMVIIDSMRAS1m_k{mynumber}.zip!/GeoTIFF/"
+
+        my_files.append(f"{mypath}DHMVIIDSMRAS1m_k{mynumber}{extension}")
+
+    return my_files
--- main.py
+++ main.py
@@ -7,10 +7,10 @@ import geopandas as gpd
 import pyproj
 from geopy.geocoders import Nominatim
 import numpy as np
-import json
+
 import plotly.graph_objects as go
 from plotly.offline import download_plotlyjs, init_notebook_mode,plot,iplot
-import cufflinks as cf
+#import cufflinks as cf
 
 
 #cf.go_offline()
@@ -165,74 +165,9 @@ def process_input(my_address)-> List:
     return sbox
 
 
-def create_list_files():
-    """
-    Create a variable that contains the liste of files that i have to read
-    """
-
-    my_files:List= []
-    mynumber:str = ""
-    extension = ".tif"
-
-    for x in range(1,44):
-        if x < 10:
-            mynumber = "0"+str(x)
-            mypath = f"zip+file:.///geo/DHMVIIDSMRAS1m_k{mynumber}.zip!/GeoTIFF/"
-        else:
-            mynumber = str(x)
-            mypath = f"zip+file:.///geo/DHMVIIDSMRAS1m_k{mynumber}.zip!/GeoTIFF/"
-
-        my_files.append(f"{mypath}DHMVIIDSMRAS1m_k{mynumber}{extension}")
-
-    return my_files
-
-
-
-def create_coord_json():
-
-    mylist = create_list_files()
-
-    data = dict()
-    data["parameters"] = []
-
-    for elem in mylist:
-
-        myfile = rio.open(elem)
-
-        path = myfile.name
-
-        file = path.split("/")[8]
-        file = file.split(".")[0]
-
-        big_left:float = myfile.bounds.left
-        big_top:float = myfile.bounds.top
-        big_bottom:float= myfile.bounds.bottom
-        big_right:float = myfile.bounds.right
-
-        data["parameters"].append({
-            "Left" : big_left,
-            "Bottom" : big_bottom,
-            "Right" : big_right,
-            "Top" : big_top,
-            "Name" : file,
-            "Path" : path
-        })
-
-    with open("mycoord.json", "w") as mydata:
-        json.dump(data,mydata)
-    
-
-def read_json():
-    with open("mycoord.json") as file:
-        data = json.load(file)
-    
-    return data["parameters"]
-
-
-
 def inside_files(small_box):
 
-    data = read_json()
+    data = myJson.read_json()
 
     result = 0
 
